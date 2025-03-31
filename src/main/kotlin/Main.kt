9edb9bbaaac9d5057aff059e6db60cbfd6399bcd
Aleksandr Volynski <al.volynski@ariscard.net>
2025-03-31 11:33:22 +0300

    get quotes from content

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,15 @@
-package org.ks.lesson
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
 
 fun main() {
-    println("Hello World!")
+    val url = "https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/"
+    val doc: Document = Jsoup.connect(url).get()
+    val elements: Elements = doc.select(QUOTE_CSS_CLASS)
+
+    for(element in elements) {
+        println(element.text())
+    }
 }
+
+const val QUOTE_CSS_CLASS = ".sc-2aegk7-2"
